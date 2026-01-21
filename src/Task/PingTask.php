f1ea9d9fd81972f6f4ec8eb066c4447d1d314542
TOBENTOch <info@tobento.ch>
2026-01-21 11:49:50 +0100

    Guzzle remove using PSR-18  / PSR-s7 instead

--- src/Task/PingTask.php
+++ src/Task/PingTask.php
@@ -13,16 +13,14 @@ declare(strict_types=1);
 
 namespace Tobento\Service\Schedule\Task;
 
-use Tobento\Service\Schedule\TaskResultInterface;
-use Tobento\Service\Schedule\TaskResult;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Client;
+use Psr\Http\Message\StreamFactoryInterface;
+use Tobento\Service\Schedule\TaskResult;
+use Tobento\Service\Schedule\TaskResultInterface;
 
-/**
- * PingTask
- */
 class PingTask extends AbstractTask
 {
     /**
@@ -35,12 +33,16 @@ class PingTask extends AbstractTask
      *
      * @param string $uri
      * @param string $method
-     * @param array $options
+     * @param array $query
+     * @param array $headers
+     * @param string|null $body
      */
     public function __construct(
         protected string $uri,
         protected string $method = 'GET',
-        protected array $options = [],
+        protected array $query = [],
+        protected array $headers = [],
+        protected string|null $body = null,
     ) {}
 
     /**
@@ -52,17 +54,42 @@ class PingTask extends AbstractTask
      */
     public function processTask(ContainerInterface $container): TaskResultInterface
     {
-        $client = $container->has(ClientInterface::class)
-            ? $container->get(ClientInterface::class)
-            : new Client();
+        // Resolve PSR-18 client
+        $client = $container->get(ClientInterface::class);
+
+        // Resolve PSR-17 request factory
+        $factory = $container->get(RequestFactoryInterface::class);
         
-        $this->response = $client->request(
-            method: $this->getMethod(),
-            uri: $this->getUri(),
-            options: $this->getOptions(),
+        // Build URI with query parameters
+        $uri = $this->uri;
+
+        if (!empty($this->getQuery())) {
+            $queryString = http_build_query($this->getQuery());
+            $uri .= (str_contains($uri, '?') ? '&' : '?') . $queryString;
+        }
+        
+        // Build request
+        $request = $factory->createRequest($this->method, $uri);
+
+        // Add headers if provided
+        foreach($this->getHeaders() as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        // Add body if provided
+        if ($this->getBody() !== null) {
+            $streamFactory = $container->get(StreamFactoryInterface::class);
+            $stream = $streamFactory->createStream($this->getBody());
+            $request = $request->withBody($stream);
+        }
+
+        // Send request
+        $this->response = $client->sendRequest($request);
+
+        return new TaskResult(
+            task: $this,
+            output: (string) $this->response->getBody()
         );
-        
-        return new TaskResult(task: $this, output: (string)$this->response->getBody());
     }
     
     /**
@@ -98,15 +125,35 @@ class PingTask extends AbstractTask
     {
         return $this->method;
     }
-    
+
     /**
-     * Returns the options.
+     * Returns the query.
      *
      * @return array
      */
-    public function getOptions(): array
+    public function getQuery(): array
     {
-        return $this->options;
+        return $this->query;
+    }
+
+    /**
+     * Returns the headers.
+     *
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+    
+    /**
+     * Returns the body.
+     *
+     * @return null|string
+     */
+    public function getBody(): null|string
+    {
+        return $this->body;
     }
     
     /**
